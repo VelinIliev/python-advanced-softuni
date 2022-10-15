839b377b17221771b46cfe4fb51017293201ca69
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-10-15 13:07:09 +0300

    Multidimensional Lists - Exercise 1

--- 07_multidimensional_lists_exercise1/10_radioactive_mutate_vampire_bunnies.py
+++ 07_multidimensional_lists_exercise1/10_radioactive_mutate_vampire_bunnies.py
@@ -1,1 +1,26 @@
-# TODO: not ready
+rows, cols = [int(x) for x in input().split()]
+
+matrix = []
+player_position = []
+bunnies_positions = []
+
+directions = {
+    "L": [0, -1],
+    "R": [0, 1],
+    "U": [1, 0],
+    "D": [-1, 0]
+}
+
+for row in range(rows):
+    new_row = [x for x in input()]
+    if "P" in new_row:
+        player_position = [row, new_row.index("P")]
+
+    matrix.append(new_row)
+
+for x in matrix:
+    print(x)
+print(player_position)
+print(bunnies_positions)
+
+#TODO: not ready
