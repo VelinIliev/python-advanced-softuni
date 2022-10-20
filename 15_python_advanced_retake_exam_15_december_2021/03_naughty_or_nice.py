067a1c45e65d8d52cfcbec6e2065b9d1b72b579d
VelinIliev <veliniliev@abv.bg>
2022-10-20 15:11:09 +0300

    finished

--- 15_python_advanced_retake_exam_15_december_2021/03_naughty_or_nice.py
+++ 15_python_advanced_retake_exam_15_december_2021/03_naughty_or_nice.py
@@ -1,20 +1,44 @@
 def naughty_or_nice_list(*args, **kwargs):
-    final_dict = {
+    kids = {
         "Nice": [],
         "Naughty": [],
         "Not found": []
     }
-    kids = {}
-    for x in args[0]:
-        number = int(x[0])
-        name = x[1]
-        if number not in kids:
-            kids[number] = [name]
-        else:
-            kids[number].append(name)
-    print(kids)
+    santa_list = args[0]
+    for i in range(1, len(args)):
+        number, name = args[i].split("-")
+        number = int(number)
+        count = 0
+        for kid in santa_list:
+            if kid[0] == number:
+                count += 1
+        if count == 1:
+            for x, kid in enumerate(santa_list):
+                if kid[0] == number:
+                    kids[name].append(kid[1])
+                    santa_list.pop(x)
+    for name, key in kwargs.items():
+        count = 0
+        for kid in santa_list:
+            if name == kid[1]:
+                count += 1
+        if count == 1:
+            for y, kid in enumerate(santa_list):
+                if name == kid[1]:
+                    kids[key].append(name)
+                    santa_list.pop(y)
+                pass
+    for x, name in santa_list:
+        kids["Not found"].append(name)
+    final_string = ""
 
-    pass
+    for type, names in kids.items():
+        if names:
+            if type != "Not found":
+                final_string += f'{type}: {", ".join(names)}\n'
+            else:
+                final_string += f'{type}: {", ".join(names)}'
+    return final_string
 
 
 print(naughty_or_nice_list(
@@ -29,5 +53,36 @@ print(naughty_or_nice_list(
     Amy="Nice",
     Katy="Naughty",
 ))
-
-#TODO:not ready
+print()
+print(naughty_or_nice_list(
+    [
+        (7, "Peter"),
+        (1, "Lilly"),
+        (2, "Peter"),
+        (12, "Peter"),
+        (3, "Simon"),
+    ],
+    "3-Nice",
+    "5-Naughty",
+    "2-Nice",
+    "1-Nice",
+    ))
+print()
+print(naughty_or_nice_list(
+    [
+        (6, "John"),
+        (4, "Karen"),
+        (2, "Tim"),
+        (1, "Merry"),
+        (6, "Frank"),
+    ],
+    "6-Nice",
+    "5-Naughty",
+    "4-Nice",
+    "3-Naughty",
+    "2-Nice",
+    "1-Naughty",
+    Frank="Nice",
+    Merry="Nice",
+    John="Naughty",
+))
