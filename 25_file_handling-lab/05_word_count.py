0d2dc8dcdff726fbad9d2f43933a2dd324d66bf4
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-01-29 12:40:24 +0200

    refactor

--- 25_file_handling-lab/05_word_count.py
+++ 25_file_handling-lab/05_word_count.py
@@ -7,26 +7,20 @@ for line in first_file:
     for word in line.split():
         words[word] = 0
 
-print(words)
 
 second_file = open('input.txt', 'r')
 
 for line in second_file:
     for word in line.split():
-        word = word.replace("-", "")
-        word = word.replace(".", "")
-        word = word.replace(",", "")
-        word = word.replace("?", "")
-        word = word.replace("!", "")
+        word = word.replace("-", "").replace(".", "").replace(",", "").replace("?", "").replace("!", "")
         word = word.lower()
-        # print(word)
         if word in words:
             words[word] += 1
 
 sorted_file = list(sorted(words.items(), key=lambda x: -x[1]))
-print(sorted_file)
 
 
 with open('output.txt', 'w') as f:
     for k, v in sorted_file:
+        print(f"{k} - {v}")
         f.write(f"{k} - {v}\n")
