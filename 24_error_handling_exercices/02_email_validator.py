738844c270aff074805c37f2055410b59bdc2266
VelinIliev <veliniliev@gmail.com>
2023-02-01 12:19:13 +0200

    refactor

--- 24_error_handling_exercices/02_email_validator.py
+++ 24_error_handling_exercices/02_email_validator.py
@@ -1,3 +1,6 @@
+import re
+
+
 class NameTooShortError(BaseException):
     """Name must be more than 4 characters"""
     pass
@@ -18,20 +21,39 @@ class InvalidProviderError (BaseException):
     pass
 
 
+class TooManyAtSymbolsError (BaseException):
+    """You have entered too many @"""
+    pass
+
+
+class InvalidSymbolsError (BaseException):
+    """You have entered too many @"""
+    pass
+
+
+pattern = r'[.@\w-]+'
+
 line = input()
 
 while line != 'End':
+    matches = re.findall(pattern, line)
+    if len(matches) > 1:
+        raise InvalidSymbolsError("Email must contain only letters, numbers, '@', '_',  '-' or '.'")
+
     data = line.split("@")
-    name = data[0]
-    if len(name) <= 4:
-        raise NameTooShortError('Name must be more than 4 characters')
+    if len(data) > 2:
+        raise TooManyAtSymbolsError('You have entered too many @')
     if len(data) == 1:
         raise MustContainAtSymbolError("Email must contain @")
 
-    domain = data[1].split(".")
-    if len(domain) == 1 or len(domain[0]) == 0:
+    name = data[0]
+    if len(name) <= 4:
+        raise NameTooShortError('Name must be more than 4 characters')
+
+    provider, *domain = data[1].split(".")
+    if len(provider) == 0:
         raise InvalidProviderError("You must have a provider")
-    if domain[1] not in ['com', 'bg', 'org', 'net']:
+    if domain[-1] not in ['com', 'bg', 'org', 'net']:
         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
 
     print('Email is valid')
